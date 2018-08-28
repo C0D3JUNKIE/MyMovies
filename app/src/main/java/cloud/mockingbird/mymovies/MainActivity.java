cef63bd1663e9a6cc42ba2192af1bc1981445d27
Albert Bustamante <albertbustamante@Alberts-MBP.lan>
2018-08-27 21:19:15 -0500

    Second commit - adding small changes to mainActivity

--- app/src/main/java/cloud/mockingbird/mymovies/MainActivity.java
+++ app/src/main/java/cloud/mockingbird/mymovies/MainActivity.java
@@ -4,14 +4,29 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
+
+  private RecyclerView recyclerView;
+  private TextView errorMessageDisplay;
+  private ProgressBar loadingIndicator;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    recyclerView = (RecyclerView) findViewById(R.id.rv_movie_posters);
+    errorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+
+
+
   }
 
   @Nullable
